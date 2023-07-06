9ccb281e30834f52bd88b375edbe11597556c27e
Satyam Sinha <satyamsinha9404@gmail.com>
2023-07-07 03:20:27 +0530

    Add mobile layout view

--- lib/presentation/screens/mobile_layout/mobile_view.dart
+++ lib/presentation/screens/mobile_layout/mobile_view.dart
@@ -2,7 +2,10 @@ import 'package:flutter/material.dart';
 import 'package:test_task/presentation/screens/mobile_layout/banner_mobile.dart';
 import 'package:test_task/presentation/screens/mobile_layout/custom_mobile_appbar.dart';
 import 'package:test_task/presentation/screens/mobile_layout/custom_mobile_bottombar.dart';
+import 'package:test_task/presentation/screens/mobile_layout/tabbar_mobile_screen.dart';
+import 'package:test_task/presentation/screens/web_layout/tabbar_web.dart';
 import 'package:test_task/presentation/shared/constants/constant_function.dart';
+import 'package:test_task/presentation/shared/constants/string.dart';
 
 class MobileView extends StatelessWidget {
   const MobileView({super.key});
@@ -12,11 +15,25 @@ class MobileView extends StatelessWidget {
     return Stack(
       children: [
         SingleChildScrollView(
-          child: Column(
-            children: [
-              (60).ph,
-              const BannerMobile(),
-            ],
+          child: Padding(
+            padding: const EdgeInsets.symmetric(vertical: 60),
+            child: Column(
+              children: [
+                const BannerMobile(),
+                (0.1 * deviceHeight!).ph,
+                SingleChildScrollView(
+                  scrollDirection: Axis.horizontal,
+                  child: Row(
+                    children: [
+                      24.pw,
+                      const TabbarWeb(),
+                      24.pw
+                    ],
+                  ),
+                ),
+                const TabbarMobileScreen()
+              ],
+            ),
           ),
         ),
         const Column(
---
+++ lib/presentation/screens/mobile_layout/tabbar_mobile_screen.dart
@@ -0,0 +1,110 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:provider/provider.dart';
+import 'package:test_task/presentation/shared/constants/constant_function.dart';
+import 'package:test_task/presentation/shared/constants/string.dart';
+
+import '../../../controller/tab_controller.dart';
+import '../../../model/tab_model.dart';
+import '../../shared/constants/app_colors.dart';
+
+class TabbarMobileScreen extends StatelessWidget {
+  const TabbarMobileScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final tabBarController = Provider.of<TabBarController>(context);
+    final myModel = dummyData[tabBarController.selectedTabIndex];
+    return Column(
+      children: [
+        (0.04 * deviceHeight!).ph,
+        Text(
+          myModel.title,
+          style: Theme.of(context)
+              .textTheme
+              .headlineSmall!
+              .copyWith(color: bodyTitleColor),
+          textAlign: TextAlign.center,
+        ),
+        (0.04 * deviceHeight!).ph,
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.end,
+          children: [
+            Text(
+              "1.",
+              style: Theme.of(context)
+                  .textTheme
+                  .displayLarge!
+                  .copyWith(color: bodyNumColor, fontSize: 100),
+            ),
+            Column(
+              children: [
+                SvgPicture.asset(myModel.image1, height: 0.15 * deviceHeight!),
+                Padding(
+                  padding: const EdgeInsets.all(24.0),
+                  child: Text(myModel.text1),
+                ),
+              ],
+            ),
+          ],
+        ),
+        (0.05 * deviceHeight!).ph,
+        Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              colors: [bodyMidCol1, bodyMidCol2],
+              begin: Alignment.centerLeft,
+              end: Alignment.centerRight,
+            ),
+          ),
+          child: Padding(
+            padding: EdgeInsets.all(deviceHeight! * 0.05),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.end,
+              children: [
+                Row(
+                  crossAxisAlignment: CrossAxisAlignment.end,
+                  children: [
+                    Text(
+                      "2.",
+                      style: Theme.of(context)
+                          .textTheme
+                          .displayLarge!
+                          .copyWith(color: bodyNumColor, fontSize: 100),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(24.0),
+                      child: Text(myModel.text2),
+                    ),
+                  ],
+                ),
+                SvgPicture.asset(myModel.image2),
+              ],
+            ),
+          ),
+        ),
+        (0.05 * deviceHeight!).ph,
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.end,
+          children: [
+            Text(
+              "3.",
+              style: Theme.of(context)
+                  .textTheme
+                  .displayLarge!
+                  .copyWith(color: bodyNumColor, fontSize: 100),
+            ),
+            (deviceWidth! * 0.1).pw,
+            SizedBox(width: deviceWidth! * 0.5, child: Text(myModel.text3)),
+          ],
+        ),
+            SvgPicture.asset(myModel.image3),
+      ],
+    );
+  }
+}
+
+
